96a4d22fe0e520cb7b33f9c99514cb94378b776b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-08-23 23:36:44 +0100

    Fix separator bug in convertCase function

--- src/string/index.ts
+++ src/string/index.ts
@@ -3,37 +3,42 @@
  */
 
 export type StringCase = "camel" | "kebab" | "pascal" | "snake";
-export type Separator = string | string[] | RegExp;
+export type Separator = string | RegExp;
 
 /**
  * 
  * @param {string} string The string to be converted
  * @param {StringCase} to The case to convert the string to
+ * @param {Separator} separator? Custom separator for complex strings
  * @returns {string} The converted string
  */
 
 export function convertCase(string: string, to: StringCase, separator?: Separator): string {
+  if (typeof string !== "string") throw new Error(`Invalid argument. ${string} is not a string.`);
+  if (!["kebab","camel","pascal","snake"].includes(to)) throw new Error(`Invalid argument. ${to} is not a valid string case.`);
+  if (separator && typeof separator !== "string" && !(separator instanceof RegExp)) throw new Error(`Invalid argument. ${separator} is not a string or regular expression.`);
+
+  const separatorIsString = !!separator && typeof separator === "string";
+
+  const sanitizedSeparator = separator && (separator as string).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
   let searchString = separator 
-    ? separator instanceof RegExp
-    ? separator
-    : new RegExp(`${
-        Array.isArray(separator) 
-          ? separator.join("|")
-          : separator
-      }`, 'g') 
-    : /^[A-Z]|^[a-z]|[A-Z]|-[a-z]|_[a-z]| [A-Z]| [a-z]/g,
-  replaceString = (c: string) => {return c};
+    ? separatorIsString
+      ? new RegExp(`^[a-z]|^[A-Z]|${sanitizedSeparator}[a-z]|${sanitizedSeparator}[A-Z]`, "g")
+      : new RegExp(`^[a-z]|^[A-Z]|${(separator as RegExp).source}[a-z]|${(separator as RegExp).source}[A-Z]`, "g")
+    : /^[a-z]|^[A-Z]|[A-Z]|-[a-z]|_[a-z]| [A-Z]| [a-z]/g,
+  replaceString = (c: string, offset?: number) => {return c};
+
   switch (to) {
     case "camel":
-      replaceString = (c: string) => {
-        if (string[0] === c) return c.toLocaleLowerCase();
+      replaceString = (c: string, offset?: number) => {
+        if (offset === 0) return c[c.length - 1].toLocaleLowerCase();
         return c[c.length - 1].toLocaleUpperCase();
       };
       break;
     case "kebab":
-      replaceString = (c: string) => {
-        if (string[0] === c) return c.toLocaleLowerCase();
+      replaceString = (c: string, offset?: number) => {
+        if (offset === 0) return c[c.length - 1].toLocaleLowerCase();
         return `-${c[c.length - 1].toLocaleLowerCase()}`;
       };
       break;
@@ -43,8 +48,8 @@ export function convertCase(string: string, to: StringCase, separator?: Separato
       };
       break;
     case "snake":
-      replaceString = (c: string) => {
-        if (string[0] === c) return c.toLocaleLowerCase();
+      replaceString = (c: string, offset?: number) => {
+        if (offset == 0) return c[c.length - 1].toLocaleLowerCase();
         return `_${c[c.length - 1].toLocaleLowerCase()}`;
       };
       break;
