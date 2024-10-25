f1e13515d15f66fe4180c6be7b872b1848d4cd88
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-25 06:05:03 +0100

    Write creatElement tests. Test for creating the element, including custom elements

--- src/dom/tests/createElement.test.ts
+++ src/dom/tests/createElement.test.ts
@@ -1,4 +1,27 @@
 import { beforeAll, describe, it, expect } from "vitest";
-import { addClass, getIds, IterableElements, removeClass, style } from "..";
+import { createElement } from "..";
 
-describe("createElement", () => {})
+describe("createElement", () => {
+
+  it("creates an element with a valid tag name", () => {
+    const newP = createElement("p", {})
+    expect(newP).toBeInstanceOf(HTMLParagraphElement);
+  });
+
+  it("creates a custom element", ()=> {
+    class TestElement extends HTMLElement {
+      constructor () { super(); }
+    }
+
+    customElements.define("test-element", TestElement)
+    const testElement = createElement("test-element", {})
+    expect(testElement).toBeInstanceOf(TestElement);
+    expect(testElement).toBeInstanceOf(HTMLElement);
+  });
+
+  it("applies given attributes to the element");
+
+  it("appends given children to the element");
+
+  it("applies given styles to the element");
+})
