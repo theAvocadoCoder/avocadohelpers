b8712fc7dbf826372629642d98bff2c7b13c901e
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-26 03:08:30 +0100

    Finish writing tests for the createElement DOM function

--- src/dom/tests/createElement.test.ts
+++ src/dom/tests/createElement.test.ts
@@ -3,25 +3,51 @@ import { createElement } from "..";
 
 describe("createElement", () => {
 
-  it("creates an element with a valid tag name", () => {
+  it("returns an element with the provided tag", () => {
     const newP = createElement("p", {})
     expect(newP).toBeInstanceOf(HTMLParagraphElement);
   });
 
-  it("creates a custom element", ()=> {
+  it("works with custom elements", ()=> {
     class TestElement extends HTMLElement {
       constructor () { super(); }
     }
+    customElements.define("test-element", TestElement);
+    const testElement = createElement("test-element", {});
 
-    customElements.define("test-element", TestElement)
-    const testElement = createElement("test-element", {})
     expect(testElement).toBeInstanceOf(TestElement);
     expect(testElement).toBeInstanceOf(HTMLElement);
   });
 
-  it("applies given attributes to the element");
+  it("applies given attributes to the element", () => {
+    const input = createElement("input", {
+      attributes: [
+        ["placeholder", "I'm a test input"],
+        ["name", "testInput"]
+      ]
+    });
 
-  it("appends given children to the element");
+    expect(input.getAttribute("name")).toBe("testInput");
+    expect(input.getAttribute("placeholder")).not.toBe("not the placeholder text");
+  });
 
-  it("applies given styles to the element");
+  it("appends given children to the element", () => {
+    const div = createElement("div", {
+      children: [
+        document.createElement("p"),
+        document.createTextNode("Text Child")
+      ]
+    });
+
+    expect(div.firstChild).toBeInstanceOf(HTMLParagraphElement);
+    expect(div.textContent?.includes("Text Child")).toBeTruthy();
+  });
+
+  it("applies given styles to the element", () => {
+    const styledP = createElement("p", {
+      styles: { color: "red"}
+    })
+
+    expect(getComputedStyle(styledP).color).toBe("rgb(255, 0, 0)");
+  });
 })
